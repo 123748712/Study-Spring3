da247bcf4981e56d6da70457585172030e2833ba
123748712 <kemco01@naver.com>
2022-05-10 15:49:41 +0900

    2022.05.10 test를 통한 db데이터생성 및 권한 부여

---
+++ src/test/java/kr/or/soo/security/PasswordEncoderTest.java
@@ -0,0 +1,64 @@
+package kr.or.soo.security;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(SpringExtension.class) // Junit5(단위테스트용 프레임워크)
+@ContextConfiguration("classpath:test-context.xml") // 설정파일 읽어오기
+public class PasswordEncoderTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Test // 서버를 키지 않아도 실행될 수 있는 어노테이션
+	@Disabled
+	public void myTest() throws Exception {
+		String mySQL = "insert into mymem(userid, userpw) values(?, ?)";
+		for (int i = 1; i <= 10; i++) {
+			Connection conn = dataSource.getConnection();
+			PreparedStatement psmt = conn.prepareStatement(mySQL);
+			psmt.setNString(1, "user" + i);
+			psmt.setNString(2, passwordEncoder.encode("user"+i));
+			psmt.executeUpdate();
+			
+			psmt.close();
+			conn.close();
+		}
+	}
+	
+	@Test
+//	@Disabled // junit5 테스트실행이 되어도 실행되지 않게하는 어노테이션
+	public void myTest2() throws Exception {
+		String mySQL = "insert into myauth(userid, userauth) values(?, ?)";
+		for (int i = 1; i <= 10; i++) {
+			Connection conn = dataSource.getConnection();
+			PreparedStatement psmt = conn.prepareStatement(mySQL);
+			psmt.setNString(1, "user" + i);
+			if(i <=5) {
+				psmt.setNString(2, "ROLE_ADMIN"); // ROLE_권한
+			} else {
+				psmt.setNString(2, "ROLE_MEMBER");
+			}
+			psmt.executeUpdate();
+			
+			psmt.close();
+			conn.close();
+		}
+	}
+}
